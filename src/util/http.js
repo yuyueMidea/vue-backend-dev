2197ce21e951cb44d80d9cad5fb0fb1efd80ee92
苍穹勇敢的心 <18825237023@163.com>
2019-11-11 16:04:28 +0800

    添加加载动画loading...

--- src/util/http.js
+++ src/util/http.js
@@ -1,63 +1,67 @@
 /*
  * @Author: your name
- * @Date: 2019-11-09 19:49:10
- * @LastEditTime: 2019-11-10 16:31:50
+ * @Date: 2019-11-09 10:24:10
+ * @LastEditTime: 2019-11-11 16:03:37
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue-backend-dev\src\util\http.js
  */
+import { Promise } from "core-js";
 import axios from 'axios'
-
+//
 import { Loading } from 'element-ui';
 let loadingInstance =null;
-axios.interceptors.request.use( (config)=> {
-    // Do something before request is sent
-	if(config.showLoading){
-		loadingInstance = Loading.service({
-				  lock: true,
-				  text: 'Loading',
-				  spinner: 'el-icon-loading',
-				  background: 'rgba(0, 0, 0, 0.7)'
-	    });
-	}
-    return config;
-  }, (error)=> {
-    // Do something with request error
-    return Promise.reject(error);
-  });
 
-// Add a response interceptor
-axios.interceptors.response.use((response)=> {
-	if(response.config.showLoading){
-		 loadingInstance.close();
-	}
+// 超时设置
+// const service = axios.create({
+//     // 请求超时时间
+//     timeout: 5000                 
+// });
+// http request 拦截器
+// 每次请求都为http头增加Authorization字段，其内容为token
+axios.interceptors.request.use(
+    config => {
+        loadingInstance = Loading.service({
+            lock: true,
+            text: 'Loading',
+            spinner: 'el-icon-loading',
+            background: 'rgba(0, 0, 0, 0.7)'
+        });
+        return config
+    },
+    err => {
+        return Promise.reject(err);
+    }
+);
+axios.interceptors.response.use(response =>{
+	loadingInstance.close();
     return response;
-  }, (error)=> {
-	  if(error.config.showLoading){
-	  	 loadingInstance.close();
-	  }
-    // Do something with response error
+  }, error =>{
+	loadingInstance.close();
     return Promise.reject(error);
   });
 
-
-export function fetch(url,param={}){
-    return new Promise((resolve,reject)=>{
-        axios.get(url, {params:param})
-        .then(resData=>{
-            resolve(resData.data)
-        }).catch(err=>{
-            reject(err)
-        })
+// 这是get方法
+export function fetch(url, param={}){
+    return new Promise((resolve, reject)=>{
+        axios.get(url, {
+                params: param
+            })
+            .then(responsData=>{
+                resolve(responsData.data)
+            }).catch(err=>{
+                reject(err)
+            })
     })
 }
 
-export function post(url,param={}){
-    return new Promise((resolve,reject)=>{
-        axios.post(url, param).then(resData=>{
-            resolve(resData.data)
-        }).catch(err=>{
-            reject(err)
-        })
+export function post(url, data={}){
+    return new Promise((resolve, reject)=>{
+        axios.post(url, data)
+            .then(responsData=>{
+                resolve(responsData.data)
+            }).catch(err=>{
+                reject(err)
+            })
     })
 }
