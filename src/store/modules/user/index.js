407ed71cc31566750b3f4a3ba6461c3ac8b6f005
苍穹勇敢的心 <18825237023@163.com>
2019-11-11 14:19:29 +0800

    vuex设置userId

--- src/store/modules/user/index.js
+++ src/store/modules/user/index.js
@@ -1,8 +1,17 @@
+/*
+ * @Author: your name
+ * @Date: 2019-03-14 14:00:10
+ * @LastEditTime: 2019-11-11 14:10:10
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue-backend-dev\src\store\modules\user\index.js
+ */
 import Cookies from 'js-cookie'
 
 const state = {
     // 用户名
-    name: ''
+    name: '',
+    id:''
 }
 
 const mutations = {
@@ -15,6 +24,16 @@ const mutations = {
             Cookies.remove('userName')
         }
         state.name = data
+    },
+    setId: (state,data) =>{
+        if(data){
+            Cookies.set('userId', encodeURIComponent(data), {
+                expires: 365
+            })
+        } else {
+            Cookies.remove('userId')
+        }
+        state.id = data
     }
 }
 
