954472c822b967145aab12b75c6cec4f8b369c46
苍穹勇敢的心 <18825237023@163.com>
2019-11-15 11:40:04 +0800

    添加多语言

--- src/lang/en.js
+++ src/lang/en.js
@@ -1,13 +1,24 @@
+/*
+ * @Author: your name
+ * @Date: 2019-03-14 14:00:10
+ * @LastEditTime: 2019-11-11 10:43:15
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue-backend-dev\src\lang\en.js
+ */
 import enElement from 'element-ui/lib/locale/lang/en'
 
 export default {
     global: {
+        age:'age',
         username: 'User Name',
         password: 'Password',
         login: 'Login',
+        register: 'register',
         captcha: 'Captcha',
+        getTel:'telphone',
         forgetPassword: 'Forget Password?',
-        loginTip: 'The login result is random. Just fill in the captcha',
+        loginTip: 'get the captcha',
         editpassword: 'Edit Password',
         logout: 'Logout',
         errMsg: {
--- src/lang/zhCN.js
+++ src/lang/zhCN.js
@@ -1,13 +1,24 @@
+/*
+ * @Author: your name
+ * @Date: 2019-03-14 14:00:10
+ * @LastEditTime: 2019-11-11 10:42:44
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue-backend-dev\src\lang\zhCN.js
+ */
 import zhElement from 'element-ui/lib/locale/lang/zh-CN'
 
 export default {
     global: {
+        age: '年龄',
         username: '用户名',
         password: '密码',
         login: '登录',
+        register: '注册',
         captcha: '验证码',
+        getTel:'请输入手机号码',
         forgetPassword: '忘记密码？',
-        loginTip: '当前登录结果随机。验证码随便填',
+        loginTip: '获取短信验证码',
         editpassword: '修改密码',
         logout: '退出登录',
         errMsg: {
